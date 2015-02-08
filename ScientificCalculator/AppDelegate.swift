9ec2fbb2e3f3f22aa0e9602906d2e11549a342d4
rocket-raccoon <the.alberto.bena@gmail.com>
2015-02-08 00:43:15 -0500

    Removed storyboard

--- ScientificCalculator/AppDelegate.swift
+++ ScientificCalculator/AppDelegate.swift
@@ -12,10 +12,14 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        var mainController = ViewController()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = mainController
+        window?.backgroundColor = .whiteColor()
         return true
     }
 
